1813d593ab5391988cc2e988f19298116bdc14e0
JanusU <janusudsen@yahoo.com>
2017-05-13 11:37:14 +0200

    rest and ourAirline

---
+++ Backend/semesterprojectBAckend/src/main/java/com/company/Airline.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company;
+
+import java.util.List;
+
+/**
+ *
+ * @author Janus
+ */
+public class Airline {
+    public String url;
+    
+    public List<Flight> flights;
+}
